4903d6369b65fcd4b01ef9999e0e9adfd172c9c9
MTECHDevelopment <mateusg@sempreceub.com>
2025-09-19 16:02:38 -0300

    .

---
+++ Venda.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+public class Venda {
+    private int id;
+    private Date data;
+    private List<ItemVenda> itens;
+    private double valorTotal;
+
+    public Venda (int id, Date data) {
+        this.id = id;
+        this.data = data;
+        this.itens = new ArrayList<>();
+    }
+
+    public void adicionarProduto (Produto p, int qtd) {
+        ItemVenda item = new ItemVenda (p, qtd);
+        itens.add(item);
+        calcularTotal();
+    }
+    public double calcularTotal () {
+        valorTotal = 0;
+        for (ItemVenda item : itens) {
+            valorTotal += item.getSubtotal();
+        }
+    };
+    public double getValorTotal() { return valorTotal; };
+}
